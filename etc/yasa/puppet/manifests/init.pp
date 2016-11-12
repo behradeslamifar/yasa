86b1a66f34463201119db11646a1ee5d0dcdc447
Behrad Eslamifar <b.eslamifar@cvak.ir>
2016-11-12 13:45:42 +0330

    add report and resolve many bug

--- etc/yasa/puppet/manifests/init.pp
+++ etc/yasa/puppet/manifests/init.pp
@@ -1,15 +1,24 @@
-# site.pp must exist (puppet #15106, foreman #1708)
-# node /.*\.cvak\.local/ {
+class yasa 
+{
   package { 'rsyslog':
     ensure => present,
   }
   
+  file { '/sbin/yasa':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0550',
+    source  => "puppet:///modules/yasa/yasa",
+  }
+ 
   file { '/etc/rsyslog.d/usblock.conf':
     notify  => Service['rsyslog'],
+    ensure  => present,
     owner   => 'root',
     group   => 'root',
     mode    => '0440',
-    source  => "puppet:///files/hosts/$::fqdn/etc/yasalog.conf",
+    source  => "puppet:///modules/yasa/hosts/$::fqdn/etc/yasalog.conf",
   }
 
   service { 'rsyslog':
@@ -19,30 +28,32 @@
   }
   
   file { '/etc/udev/rules.d/10-usblock.rules':
+    ensure => present,
     owner  => 'root',
     group  => 'root',
     mode   => '0440',
-    source => "puppet:///files/hosts/$::fqdn/etc/10-usblock.rules",
-    notify  => Exec['puppet_trigger_udev'],
-    notify  => Exec['puppet_trigger_subsystem'],
-    notify  => Exec['puppet_trigger_devices'],
+    source => "puppet:///modules/yasa/hosts/$::fqdn/etc/10-usblock.rules",
+    audit  =>  'content',
+    notify => Exec['puppet_trigger_udev'],
   }
 
   exec { 'puppet_trigger_udev':
-    command     => "bash -c 'udevadm control --reload'",
+    command     => "/sbin/udevadm control --reload; /sbin/udevadm trigger -c add -t devices -s usb",
     subscribe   => File['/etc/udev/rules.d/10-usblock.rules'],
-    refreshonly => true,
+    #refreshonly => true,
   }
 
-  exec { 'puppet_trigger_subsystem':
-    command     => "bash -c 'udevadm trigger -c add -s usb'",
-    subscribe   => File['/etc/udev/rules.d/10-usblock.rules'],
-    refreshonly => true,
-  }
-
-  exec { 'puppet_trigger_devices':
-    command     => "bash -c 'udevadm trigger -c add -t devices'",
-    subscribe   => File['/etc/udev/rules.d/10-usblock.rules'],
-    refreshonly => true,
-  }
-   
+#    notify  => Exec['puppet_trigger_subsystem'],
+#  exec { 'puppet_trigger_subsystem':
+#    command     => "/sbin/udevadm trigger -c add -s usb",
+#    subscribe   => Exec['puppet_trigger_udev'],
+#    refreshonly => true,
+#    notify  => Exec['puppet_trigger_devices'],
+#  }
+#
+#  exec { 'puppet_trigger_devices':
+#    command     => "/sbin/udevadm trigger -c add -t devices",
+#    subscribe   => Exec['puppet_trigger_subsystem'],
+#    refreshonly => true,
+#  }
+}
