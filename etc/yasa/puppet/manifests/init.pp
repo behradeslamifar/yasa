e13035f07382d20f14a551be190b2e96d7c7922b
Behrad Eslamifar <b.eslamifar@cvak.ir>
2017-12-19 08:02:57 +0330

    befor group generation

--- etc/yasa/puppet/manifests/init.pp
+++ etc/yasa/puppet/manifests/init.pp
@@ -4,29 +4,40 @@ class yasa
     ensure => present,
   }
   
-  file { '/sbin/yasa':
+  service { 'rsyslog':
+    ensure  => 'running',
+    enable  => 'true',
+    require => Package['rsyslog'],
+  }
+ 
+  file { ['/usr/lib/yasa','/usr/lib/yasa/sbin']:
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0750',
+    recurse => true,
+  }  
+
+  file { '/usr/lib/yasa/sbin/yasa':
     ensure  => present,
     owner   => 'root',
     group   => 'root',
     mode    => '0550',
     source  => "puppet:///modules/yasa/yasa",
+    require => File['/usr/lib/yasa/sbin'],
   }
  
-#  file { '/etc/rsyslog.d/yasa.conf':
-#    notify  => Service['rsyslog'],
-#    ensure  => present,
-#    owner   => 'root',
-#    group   => 'root',
-#    mode    => '0440',
-#    source  => "puppet:///modules/yasa/hosts/$::fqdn/etc/yasalog.conf",
-#  }
+  file { '/etc/rsyslog.d/yasa.conf':
+    notify  => Service['rsyslog'],
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0440',
+    content => template('yasa/yasa.conf.erb'),
+    #source  => "puppet:///modules/yasa/hosts/$::fqdn/etc/yasalog.conf",
+  }
 
-#  service { 'rsyslog':
-#    ensure  => 'running',
-#    enable  => 'true',
-#    require => Package['rsyslog'],
-#  }
-  
+ 
   file { '/etc/udev/rules.d/10-yasa.rules':
     ensure => present,
     owner  => 'root',
